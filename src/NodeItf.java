f27e8e15f6cb79bce2cf7fc674f69a3b2ad1d9c1
Benjamin <benjamin.longa@gmail.com>
2019-03-21 16:07:44 +0100

    Table ARP, changement des methodes send/follow

--- src/NodeItf.java
+++ src/NodeItf.java
@@ -1,11 +1,13 @@
 import java.rmi.*;
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public interface NodeItf extends Remote {
 	public void send(int idTarget, String message) throws RemoteException;
-	public void follow(int idSender, int idTarget, ArrayList<Integer> idsFrom, String message, long idMessage) throws RemoteException;
 	public void addNeighbour(NodeItf neighbour) throws RemoteException;
 	public int getId() throws RemoteException;
 	public ArrayList<NodeItf> getNeighbours() throws RemoteException;
-	public void acknowledge(Ack ack, ArrayList<Integer> idsFrom) throws RemoteException;
+	public void setup() throws RemoteException;
+	public void setupSend(int idSender, ArrayList<Integer> idsFrom) throws RemoteException;
+	public Hashtable<Integer, Integer> getNodeToTransfer() throws RemoteException;
 }
