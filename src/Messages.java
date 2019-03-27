f795b31558a18857ddf7f8963715080523b6402a
Mouataz <mouataz.amal@gmail.com>
2019-03-27 17:19:41 +0100

    Message reveived : done

---
+++ src/Messages.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class Messages extends Observable {
+    private ArrayList<String> archives;
+
+    public Messages() {
+        this.archives = new ArrayList<>();
+    }
+
+    public ArrayList<String> getArchives() {
+        return archives;
+    }
+
+    public void setArchives(ArrayList<String> archives) {
+        this.archives = archives;
+    }
+
+    public void addMessage(String msg){
+        this.archives.add(msg);
+        setChanged();
+        notifyObservers(msg);
+    }
+
+    public String getRecentMessage(){
+        return this.archives.get(this.archives.size() - 1);
+    }
+}
--- src/VNode.java
+++ src/VNode.java
@@ -1,18 +1,32 @@
-import java.rmi.*;
+import javafx.collections.ObservableList;
 
-public class VNode implements VNodeItf {
+import java.rmi.*;
+import java.util.Observable;
+import java.util.Observer;
+
+public class VNode implements VNodeItf, Observer {
 
     private int id;
 	private NodeItf physNode;
 	private VNodeItf right;
 	private VNodeItf left;
-
-	public VNode (int id, Node physNode, VNodeItf right, VNodeItf left) {
+	private  Messages messages;
+	public VNode (int id, Node physNode) {
 		this.id = id;
 		this.physNode = physNode;
-        this.right = right;
-        this.left = left;
+        this.right = null;
+        this.left = null;
+        this.messages = new Messages();
+        this.messages.addObserver(this);
+
 	}
+
+	public void setup(VNodeItf right,VNodeItf left){
+		this.right = right;
+		this.left = left;
+	}
+
+
 
 	public void sendRight(int idTarget, String message) throws RemoteException {
 		if (idTarget == id) {
@@ -30,22 +44,6 @@ public class VNode implements VNodeItf {
 		}
 	}
 
-	public void vFollowRight(int idSender, int idTarget, String message) throws RemoteException {
-		if (idTarget == id) {
-			System.out.println("VNode" + id + " received from VNode" + idSender + " : " + message);
-		} else {
-			//physNode.follow()
-			right.vFollowRight(id, idTarget, message);
-		}
-	}
-
-	public void vFollowLeft(int idSender, int idTarget, String message) throws RemoteException {
-		if (idTarget == id) {
-			System.out.println("VNode" + id + " received from VNode" + idSender + " : " + message);
-		} else {
-			left.vFollowLeft(id, idTarget, message);
-		}
-	}
 
 	public void setRight (VNodeItf right) throws RemoteException {
 		this.right = right;
@@ -65,5 +63,14 @@ public class VNode implements VNodeItf {
 
 	public NodeItf getPhysNode()throws RemoteException {
 		return physNode;
+	}
+
+	public Messages getMessages() {
+		return messages;
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		System.out.println("i get the message " + this.messages.getRecentMessage());
 	}
 }
